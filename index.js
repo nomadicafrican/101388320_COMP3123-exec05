e28d80748eb505c874fae977cb44eb0a87df8557
Ammar <ammar.kusow2@gmail.com>
2024-10-06 15:22:32 -0400

    completed exec05

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const router = express.Router();
 
@@ -7,15 +7,24 @@ const router = express.Router();
 - add <h1> tag with message "Welcome to ExpressJs Tutorial"
 - Return home.html page to client
 */
-router.get('/home', (req,res) => {
-  res.send('This is home router');
+const path = require("path");
+
+router.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "home.html"));
 });
 
 /*
 - Return all details from user.json file to client as JSON format
 */
-router.get('/profile', (req,res) => {
-  res.send('This is profile router');
+const fs = require("fs");
+const { json } = require("express/lib/response");
+router.get("/profile", (req, res) => {
+  fs.readFile("user.json", "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "unable to read file" });
+    }
+    res.json(JSON.parse(data));
+  });
 });
 
 /*
@@ -37,28 +46,52 @@ router.get('/profile', (req,res) => {
         message: "Password is invalid"
     }
 */
-router.post('/login', (req,res) => {
-  res.send('This is login router');
+
+router.post("/login", express.json(), (req, res) => {
+  const { username, password } = req.body;
+
+  fs.readFile("user.json", "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "unable to read file" });
+    }
+
+    const user = JSON.parse(data);
+
+    if (user.username !== username) {
+      return res
+        .status(400)
+        .json({ status: false, error: "username is invalid" });
+    }
+
+    if (user.password !== password) {
+      return res
+        .status(400)
+        .json({ status: false, error: "password is invalid" });
+    }
+
+    return res.json({ status: true, message: "user is valid" });
+  });
 });
 
 /*
 - Modify /logout route to accept username as parameter and display message
     in HTML format like <b>${username} successfully logout.<b>
 */
-router.get('/logout', (req,res) => {
-  res.send('This is logout router');
+router.get("/logout/:username", (req, res) => {
+  const username = req.params.username;
+  res.send(`<b>${username} successfully logged out.</b>`);
 });
 
 /*
 Add error handling middleware to handle below error
 - Return 500 page with message "Server Error"
 */
-app.use((err,req,res,next) => {
-  res.send('This is error router');
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: "This is error router" });
 });
 
-app.use('/', router);
+app.use("/", router);
 
 app.listen(process.env.port || 8081);
 
-console.log('Web Server is listening at port '+ (process.env.port || 8081));
+console.log("Web Server is listening at port " + (process.env.port || 8081));
